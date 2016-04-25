dd93e66bf0c6db020f7f213e69798ec23904da47
Felipe Roscoche <fer8@pitt.edu>
2016-04-24 20:43:00 -0400

    One Screen Only
    changed sections for one screen only table

--- www/js/myfuncs.js
+++ www/js/myfuncs.js
@@ -1,50 +1,48 @@
-var page=0;
-var max=0;
 $(document).ready(function(){
 				buildForm("#main");
-				//buildForm("#main2");
-				if($(location).attr('href').indexOf("#section0")==-1){
-					$(location).attr('href','#section0');
-				}else{
-					$(location).attr('href','');
-				}
-				$(document).on("swipeleft", function(){
-					pageControl(1);
-					});
-				$(document).on("swiperight", function(){
-					pageControl(-1);
-				});
+				
 			});
 
-function pageControl(control){
-		
-		if(control>0&&page<(max-1)) page+=control;
-		if(control<0&&page>0) page+=control;
-		$(location).attr('href','#section'+page);
 	
-}		
 function buildForm(div){
 	var path=window.location.href.replace('index.html','');
-				$.getJSON(path+"form.json",function(data){
+				$.getJSON(path+"js/form.json",function(data){
+					$.getJSON(path+"js/caldata.json", function(calendarData){
 					
-					$table="";
-					
-					for(i=0;i<data.topics.length;i++){
 						$table="";
-						$table+="<section height='100%' data-role='page' id='section"+i+"'><div data-role='header'><h1><b>EventHistoryCalendar</b></h1></div><table width='80%'  align='center' border='0' id='table"+i+"'>";
-						$table+="<tr><td colspan='2'><h1>"+data.topics[i].topicLabel+"</h1></td><td></td></tr>";
-						for(j=0;j<data.timeline.length;j++){
-						$table+="<tr><td width='15%'>"+data.timeline[j].timeItemLabel+"</td><td><input type='text'/></td></tr>";
+						$table+="<table>";
+						
+						//CREATING ROW AND FIRST CELL EMPTY
+						$table+="<tr><th></th>";
+						
+						//CREATING TABLE HEADERS USING THE TIMELINE ITEMS
+						for(i=0;i<data.timeline.length;i++){
+							$table+="<th>"+data.timeline[i].timeItemLabel+"</th>";
 						}
-						$table+="<tr><td><a onClick='pageControl(-1);' style='float:left;' data-role='button'>Previous</a></td><td><a onClick='pageControl(1);' style='float:right;' data-role='button'>Next</a></td></tr><table><div data-role='footer'><h1><b>EventHistoryCalendar</b></h1></div></section>";
-						$("body").append($table);
+						$table+="</tr>";
 						
-						$("#table"+i).css("margin","auto");
+						//NOW WE ARE CREATING THE ROWS WITH THE QUESTIONS AND EMPTY CELLS
+						for(j=0;j<data.topics.length;j++){
+							$table+="<tr>";
+							for(i=0;i<=data.timeline.length;i++){
+								if(i==0) $table+="<td class='questioncol'>"+data.topics[j].topicLabel+"</td>";
+								else $table+="<td id='"+data.topics[j].topicID+"_"+data.timeline[i-1].timeItemID+"'></td>";
+								
+							}
+							$table+="</tr>";
+						}
+						$table+="</table>";
+						$(div).append($table);
 						
-					}
-					max=data.topics.length;
+						//FINISHED BUILDING THE TABLE, NOW WE ARE GOING TO RETRIEVE AND SET QUESTIONS ALREADY ANSWERED
+						for(i=0;i<calendarData.caldata.length;i++){
+							$("#"+calendarData.caldata[i].topicID+"_"+calendarData.caldata[i].timeItemID).text(calendarData.caldata[i].textValue);
+						}
+						
 					
 					
+					
+					});
 				});
 			 
 };
